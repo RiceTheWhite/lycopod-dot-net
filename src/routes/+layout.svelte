f41a5078f4cb1878d9d4a6999304d3965af51952
lycopod <luongcaophong1312@gmail.com>
2026-01-28 22:05:13 +0700

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import '@fontsource/poppins';
+
+	import favicon from '$lib/assets/favicon.svg';
+	import '../app.css'
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<div class="container">
+	{@render children()}
+</div>
+
+<style>
+	.container {
+		max-width: 70em;
+		margin: auto;
+	}
+</style>
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,2 @@
+<h1>under contruction...</h1>
+<h2>see old site?</h2>
